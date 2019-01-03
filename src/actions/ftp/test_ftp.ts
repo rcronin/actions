a16137cb2cce8d5d452c7ac95bb260c74ba51f33
Ryan Cronin <ryancronin23@gmail.com>
2019-01-03 08:15:15 -0500

    initial commit

--- src/actions/ftp/test_ftp.ts
+++ src/actions/ftp/test_ftp.ts
@@ -3,17 +3,20 @@ import * as sinon from "sinon"
 
 import * as Hub from "../../hub"
 
-import { SFTPAction } from "./sftp"
+import { FTPAction } from "./ftp"
 
-const action = new SFTPAction()
+const action = new FTPAction()
 
-function expectSFTPMatch(request: Hub.ActionRequest, dataMatch: any, pathMatch: any) {
+function expectFTPMatch(request: Hub.ActionRequest, dataMatch: any, pathMatch: any) {
 
   const putSpy = sinon.spy(async () => Promise.resolve())
 
-  const stubClient = sinon.stub(action as any, "sftpClientFromRequest")
+  const putEnd = sinon.spy(async () => Promise.resolve())
+
+  const stubClient = sinon.stub(action as any, "ftpClientFromRequest")
     .callsFake(() => ({
       put: putSpy,
+      end: putEnd,
     }))
 
   const stubSuggestedFilename = sinon.stub(request as any, "suggestedFilename")
@@ -37,13 +40,13 @@ describe(`${action.constructor.name} unit tests`, () => {
       request.attachment.dataBuffer = Buffer.from("1,2,3,4", "utf8")
 
       return chai.expect(action.execute(request)).to.eventually
-        .be.rejectedWith("Needs a valid SFTP address.")
+        .be.rejectedWith("Needs a valid FTP address.")
     })
 
     it("errors if the input has no attachment", () => {
       const request = new Hub.ActionRequest()
       request.formParams = {
-        address: "sftp://host/path/",
+        address: "ftp://host/path/",
       }
 
       return chai.expect(action.execute(request)).to.eventually
@@ -52,10 +55,9 @@ describe(`${action.constructor.name} unit tests`, () => {
 
     it("errors with bad paths", async () => {
       const bumAddresses = [
-        "sftp:/host/path/",
-        "ftp://host/path/",
+        "ftp:/host/path/",
         "http://host/path/",
-        "sftp/host/path/",
+        "ftp/host/path/",
         "/host/path/",
       ]
 
@@ -71,22 +73,22 @@ describe(`${action.constructor.name} unit tests`, () => {
     it("sends right body to filename and address", () => {
       const request = new Hub.ActionRequest()
       request.formParams = {
-        address: "sftp://host/path/",
+        address: "ftp://host/path/",
       }
       request.attachment = {dataBuffer: Buffer.from("1,2,3,4", "utf8")}
-      return expectSFTPMatch(request,
-        request.attachment.dataBuffer, "path/stubSuggestedFilename")
+      return expectFTPMatch(request,
+        request.attachment.dataBuffer, "/path/stubSuggestedFilename")
     })
 
     it("sends to right filename if specified", () => {
       const request = new Hub.ActionRequest()
       request.formParams = {
-        address: "sftp://host/path/",
+        address: "ftp://host/path/",
         filename: "mywackyfilename",
       }
       request.attachment = {dataBuffer: Buffer.from("1,2,3,4", "utf8")}
-      return expectSFTPMatch(request,
-        request.attachment.dataBuffer, "path/mywackyfilename")
+      return expectFTPMatch(request,
+        request.attachment.dataBuffer, "/path/mywackyfilename")
     })
 
   })
